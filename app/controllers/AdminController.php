d10e0e89b37e805f1b14bb4c57f5b82f917eb626
monsebolena <monsebaldo7@gmail.com>
2025-02-24 20:22:14 +0100

    ajuste app

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,7 +1,7 @@
 <?php
 namespace Formacom\controllers;
 use Formacom\Core\Controller;
-class HomeController extends Controller{
+class AdminController extends Controller{
     public function index(...$params){
         $data = ['mensaje' => '¡Bienvenido a la página de inicio!'];
         $this->view('home', $data);
